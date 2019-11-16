755fc3d3ec1c802d1c3dba4974a74529cfb6ec25
Kabibi <aaron.csie@gmail.com>
2019-11-16 18:51:37 +0800

    add SMIX

--- src/learners/__init__.py
+++ src/learners/__init__.py
@@ -1,7 +1,9 @@
 from .q_learner import QLearner
 from .coma_learner import COMALearner
+from .s_learner import SLearner
 
 REGISTRY = {}
 
 REGISTRY["q_learner"] = QLearner
 REGISTRY["coma_learner"] = COMALearner
+REGISTRY["s_learner"] = SLearner
